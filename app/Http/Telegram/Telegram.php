9384c4828f2547ed86fbd6b8706977ffb9708165
Abdumalik <axtrem13@gmail.com>
2022-05-13 00:30:36 +0500

    stop tg from dumping all data

--- app/Http/Telegram/Telegram.php
+++ app/Http/Telegram/Telegram.php
@@ -179,7 +179,7 @@ class Telegram{
         ];
         // dd($message);
         $this->sendMessage($message); 
-        $this->answerCallbackQuery();
+        // $this->answerCallbackQuery();
     }
     public function answerCallbackQuery(){
         if($this->data['callback_query']){
